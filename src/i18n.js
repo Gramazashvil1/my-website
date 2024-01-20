6b3a924472358ee0849520f8fbfcf71ef2a099cd
Gramazashvil1 <goga.ramazashvili@gmail.com>
2024-01-20 13:29:12 +0400

    Add: react-i18next for MultiLanguage support to the project

---
+++ src/i18n.js
@@ -0,0 +1,29 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import translationsEN from './locales/en/translation.json';
+import translationsKA from './locales/ka/translation.json';
+import LanguageDetector from 'i18next-browser-languagedetector'
+
+i18n.on('languageChanged', (lng) => {
+    document.documentElement.setAttribute('lang', lng)
+})
+i18n.use(LanguageDetector).use(initReactI18next).init({
+    supportedLngs: ['ka', 'en'],
+    whitelist: ['ka', 'en'],
+    fallbackLng: 'ka',
+    resources: {
+        en: {
+            translation: translationsEN
+        },
+        ka: {
+            translation: translationsKA
+        }
+    },
+    detection: {
+        caches: ['localStorage']
+    },
+    interpolation: {
+        escapeValue: false
+    }
+})
+export default i18n
