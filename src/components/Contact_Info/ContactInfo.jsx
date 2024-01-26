877a9c67af77807a48ca07d2f52bb975731f5410
Gramazashvil1 <goga.ramazashvili@gmail.com>
2024-01-26 18:12:46 +0400

    Add: aria labels

--- src/components/Contact_Info/ContactInfo.jsx
+++ src/components/Contact_Info/ContactInfo.jsx
@@ -9,22 +9,22 @@ function ContactInfo() {
         <div className="contact-info">
             <ul>
                 <li>
-                    <a target="_blank" rel='noopener noreferrer' href="#">
+                    <a target="_blank" rel='noopener noreferrer' aria-label="Linkedin" href="https://www.linkedin.com/in/goga-ramazashvili-1468592ab">
                         <Linkedin_img className="icon"/>
                     </a>
                 </li>
                 <li>
-                    <a href="mailto: goga.ramazashvili@gmail.com">
+                    <a href="mailto: goga.ramazashvili@gmail.com" aria-label="Email">
                         <Mail_img className="icon"/>
                     </a>
                 </li>
                 <li>
-                    <a target="_blank" rel='noopener noreferrer' href="https://github.com/Gramazashvil1">
+                    <a target="_blank" rel='noopener noreferrer' aria-label="Github" href="https://github.com/Gramazashvil1">
                         <Github_img className="icon"/>
                     </a>
                 </li>
                 <li>
-                    <a href="tel:514-322-020">
+                    <a href="tel:514-322-020" aria-label="Phone">
                         <Phone_img className="icon"/>
                     </a>
                 </li>
