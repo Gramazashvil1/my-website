e8c22e37487003d9c4e34c2731f63bd477648656
Gramazashvil1 <goga.ramazashvili@gmail.com>
2024-01-28 21:11:24 +0400

    Refactor and fix Projects component: - Update content in Projects component - Fix responsive issues - create and fill Projects.json

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,112 +1,64 @@
 import "./Projects.scss"
-import project_img_1 from '../../assets/images/project_img_1.png'
-
-import card_link_img from '../../assets/images/card_link_img.svg'
-import card_github_img from '../../assets/images/card_github_img.svg'
-
+import projectsData from '../../json/projects.json';
+import {useTranslation} from "react-i18next";
+import {useEffect, useState} from "react";
 
 function Projects() {
+
+    const {t} = useTranslation();
+
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        setProjects(projectsData);
+    }, []);
+
     return (
-        <div className="projects">
+        <div className="projects" id="projects">
             <div className="projects_main">
-                <div className="projects_header">PROJECTS</div>
+                <div className="projects_header">
+                    <h2 className="projects_title">
+                        {t('projectsTitle')}
+                    </h2>
+                </div>
+
                 <div className="projects_cards">
-
-                    <div className="projects_card">
-                        <div className="projects_card_image_frame">
-                            <img src={project_img_1} alt="Project 1"/>
+                    {projects.map(({id, title, techStackTitle, tech, live, liveLink, viewCode, viewCodeLink, image}) => (
+                        <div className="projects_card" key={id}>
+                            <div className="projects_card_image_frame">
+                                <a target="_blank" rel='noopener noreferrer' href={t(`${liveLink}`)}>
+                                    <img src={image} alt="ProjectImage" loading="lazy" width="380" height="245"/>
+                                </a>
+                            </div>
+                            <div className="card_info_top">
+                                <h3 className="card_title">
+                                    <a target="_blank" rel='noopener noreferrer' href={t(`${liveLink}`)}>
+                                        {t(`${title}`)}
+                                    </a>
+                                </h3>
+                            </div>
+                            <hr className="card_hr"/>
+                            <h4 className="card_tech_stack_title">
+                                {t(`${techStackTitle}`)}
+                            </h4>
+                            <div className="card_tech_stack_container">
+                                {tech.map((t, index) => (
+                                    <div className="card_tech_stack" key={index}>
+                                        {t}
+                                    </div>))}
+                            </div>
+                            <div className="projects_card_bottom">
+                                <a target="_blank" rel='noopener noreferrer' href={t(`${liveLink}`)}>
+                                    {t(`${live}`)}
+                                </a>
+                                {viewCodeLink &&
+                                    <a target="_blank" rel='noopener noreferrer' href={t(`${viewCodeLink}`)}>
+                                        {t(`${viewCode}`)}
+                                    </a>
+                                }
+                            </div>
                         </div>
-                        <div className="card_info_top">
-                            <p className="card_title">Personal Website</p>
-                            <p className="card_type">PORTFOLIO</p>
-                        </div>
-                        <hr className="card_hr"/>
-                        <p className="card_tech_stack_title">Tech Stack</p>
-                        <div className="card_info_bottom">
-                            <div className="card_tech_stack">React</div>
-                            <div className="card_tech_stack">JS</div>
-                            <div className="card_tech_stack">Vite</div>
-                            <div className="card_tech_stack">Scss</div>
-                            <div className="card_tech_stack">React</div>
-                            <div className="card_tech_stack">React</div>
-                        </div>
-                        <div className="projects_card_bottom">
-                            <p>
-                                <img src={card_link_img} alt="Live" />
-                                View Live
-                            </p>
-                            <p>
-                                <img src={card_github_img} alt="Live" />
-                                View Code
-                            </p>
-                        </div>
-                    </div>
-
-                    <div className="projects_card">
-                        <div className="projects_card_image_frame">
-                            <img src={project_img_1} alt="Project 1"/>
-                        </div>
-                        <div className="card_info_top">
-                            <p className="card_title">Personal Website</p>
-                            <p className="card_type">PORTFOLIO</p>
-                        </div>
-                        <hr className="card_hr"/>
-                        <p className="card_tech_stack_title">Tech Stack</p>
-                        <div className="card_info_bottom">
-                            <div className="card_tech_stack">React</div>
-                            <div className="card_tech_stack">JS</div>
-                            <div className="card_tech_stack">Vite</div>
-                            <div className="card_tech_stack">Scss</div>
-                            <div className="card_tech_stack">React</div>
-                            <div className="card_tech_stack">React</div>
-                        </div>
-                        <div className="projects_card_bottom">
-                            <p>
-                                <img src={card_link_img} alt="Live" />
-                                View Live
-                            </p>
-                            <p>
-                                <img src={card_github_img} alt="Live" />
-                                View Code
-                            </p>
-                        </div>
-                    </div>
-
-
-                    <div className="projects_card">
-                        <div className="projects_card_image_frame">
-                            <img src={project_img_1} alt="Project 1"/>
-                        </div>
-                        <div className="card_info_top">
-                            <p className="card_title">Personal Website</p>
-                            <p className="card_type">PORTFOLIO</p>
-                        </div>
-                        <hr className="card_hr"/>
-                        <p className="card_tech_stack_title">Tech Stack</p>
-                        <div className="card_info_bottom">
-                            <div className="card_tech_stack">React</div>
-                            <div className="card_tech_stack">JS</div>
-                            <div className="card_tech_stack">Vite</div>
-                            <div className="card_tech_stack">Scss</div>
-                            <div className="card_tech_stack">React</div>
-                            <div className="card_tech_stack">React</div>
-                        </div>
-                        <div className="projects_card_bottom">
-                            <p>
-                                <img src={card_link_img} alt="Live" />
-                                View Live
-                            </p>
-                            <p>
-                                <img src={card_github_img} alt="Live" />
-                                View Code
-                            </p>
-                        </div>
-                    </div>
-
-
-
-
-
+                    ))}
                 </div>
             </div>
         </div>
