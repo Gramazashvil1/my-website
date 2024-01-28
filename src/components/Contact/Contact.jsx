8c49a81325bdeb423ba5c9bc78e0c78efa60f635
Gramazashvil1 <goga.ramazashvili@gmail.com>
2024-01-28 21:18:10 +0400

    Refactor: fix skills and contact component - Update content - Fix responsive issues - create contact send forms

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,111 @@
 import "./Contact.scss"
-
+import {Form, Input, Button, notification} from "antd";
+import {useState} from "react";
+import emailjs from '@emailjs/browser';
+import ContactInfo from "../Contact_Info/ContactInfo.jsx";
+import {useTranslation} from "react-i18next";
+import contactImage from '../../assets/images/contact_img.webp';
+const {TextArea} = Input;
 
 function Contact() {
+
+    const [form] = Form.useForm();
+    const [api, contextHolder] = notification.useNotification();
+    const [loading, setLoading] = useState(false);
+    const {t} = useTranslation();
+
+    function onSuccess(type) {
+        api[type]({message: t('contactOnSuccess'), closeIcon: false, duration: 3});
+    }
+
+    function onError(type) {
+        api[type]({message: t('contactOnError'), closeIcon: false, duration: 3});
+    }
+
+    async function sendEmail(values) {
+        setLoading(true);
+        try {
+            await emailjs.send(
+                import.meta.env.VITE_EMAIL_SERVICE_ID,
+                import.meta.env.VITE_EMAIL_TEMPLATE_ID,
+                values,
+                import.meta.env.VITE_EMAIL_PUBLIC_KEY,
+            );
+            form.resetFields();
+            onSuccess('success');
+        } catch (error) {
+            console.error(error);
+            onError('error')
+        } finally {
+            setLoading(false)
+        }
+    }
+
     return (
-       <></>
+        <div className="contact" id="contact">
+            <div className="contact_main">
+                <div className="container_left">
+                    <div className="contact_img_frame">
+                        <img src={contactImage} alt="ContactImage" loading="lazy" width="350" height="280"/>
+                        <ContactInfo/>
+                    </div>
+                </div>
+
+                <div className="container_right_main">
+                    <h2 className="contact_title">
+                        {t('contactTitle')}
+                    </h2>
+                    <Form onFinish={sendEmail} form={form}>
+                        <div className="container_right_inner_left">
+                            <Form.Item name='user_name' rules={[
+                                {
+                                    required: true,
+                                    message: `${t('contactRequired')}`,
+                                },
+                            ]}>
+                                <Input placeholder={t('contactFullName')}/>
+                            </Form.Item>
+                            <Form.Item name='user_email' rules={[
+                                {
+                                    required: true,
+                                    message: `${t('contactRequired')}`,
+                                    type: 'email',
+                                },
+                            ]}>
+                                <Input placeholder={t('contactEmail')}/>
+                            </Form.Item>
+                            <Form.Item name='subject' rules={[
+                                {
+                                    required: true,
+                                    message: `${t('contactRequired')}`,
+                                },
+                            ]}>
+                                <Input placeholder={t('contactSubject')}/>
+                            </Form.Item>
+                        </div>
+
+                        <div className="container_right_inner_right">
+                            <Form.Item name='message' rules={[
+                                {
+                                    required: true,
+                                    message: `${t('contactRequired')}`,
+                                },
+                            ]}>
+                                <TextArea rows={4} placeholder={t('contactMessage')}/>
+                            </Form.Item>
+
+                            <Form.Item>
+                                {contextHolder}
+                                <Button disabled={loading} loading={loading} className="contact_button"
+                                        htmlType="submit">
+                                    {t('contactSendBtn')}
+                                </Button>
+                            </Form.Item>
+                        </div>
+                    </Form>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,30 +1,37 @@
 import "./Skills.scss"
-import skills_img from '../../assets/images/skills_img.png'
+import {useTranslation} from "react-i18next";
+import skillsImage from '../../assets/images/skills_img.webp';
+
+
+const skills = [
+    {id: 1, skillName: 'HTML'},
+    {id: 2, skillName: 'CSS'},
+    {id: 3, skillName: 'Sass'},
+    {id: 4, skillName: 'Tailwind CSS'},
+    {id: 5, skillName: 'JavaScript'},
+    {id: 6, skillName: 'React'},
+    {id: 7, skillName: 'GitHub'},
+    {id: 8, skillName: 'Ant Design'},
+    {id: 9, skillName: 'Swiper'},
+    {id: 10, skillName: 'Bootstrap'},
+    {id: 11, skillName: 'Vite'},
+];
 
 function Skills() {
+    const {t} = useTranslation()
+
     return (
-        <div className="skills">
+        <div className="skills" id="skills">
             <div className="skills-main">
-
                 <div className="skills-img-frame">
-                    <img src={skills_img} alt="skills"/>
+                    <img src={skillsImage} alt="SkillsImage" loading="lazy" width="350" height="280"/>
                 </div>
-
                 <div className="skills_right">
-                    <p className="skills_title">SKILLS</p>
+                    <h2 className="skills_title">{t('skillsTitle')}</h2>
                     <div className="skills_container">
-                        <div className="skill"></div>
-                        <div className="skill"></div>
-                        <div className="skill"></div>
-                        <div className="skill"></div>
-                        <div className="skill"></div>
-                        <div className="skill"></div>
-                        <div className="skill"></div>
-                        <div className="skill"></div>
-                        <div className="skill"></div>
-                        <div className="skill"></div>
-                        <div className="skill"></div>
-                        <div className="skill"></div>
+                        {skills.map((skill) => (
+                            <div key={skill.id} className="skill">{skill.skillName}</div>
+                        ))}
                     </div>
                 </div>
             </div>
