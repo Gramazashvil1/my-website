42deeeb15c8534cda7d3cbab3017dc017142692d
Gramazashvil1 <goga.ramazashvili@gmail.com>
2024-06-25 01:53:09 +0400

    Added Swiper carousel for improved UI/UX

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,6 +4,10 @@ import {useEffect, useState} from "react";
 import projectImage1 from '/src/assets/images/pro_img_1.webp'
 import projectImage2 from '/src/assets/images/pro_img_2.webp'
 import projectImage3 from '/src/assets/images/pro_img_3.webp'
+import {Swiper, SwiperSlide} from "swiper/react";
+import {Pagination} from "swiper/modules";
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 const projectsData = [
     {
@@ -61,42 +65,69 @@ function Projects() {
                 </div>
 
                 <div className="projects_cards">
-                    {projects.map(({id, title, techStackTitle, tech, live, liveLink, viewCode, viewCodeLink, image}) => (
-                        <div className="projects_card" key={id}>
-                            <div className="projects_card_image_frame">
-                                <a target="_blank" rel='noopener noreferrer' href={t(`${liveLink}`)}>
-                                    <img src={image} alt="ProjectImage" loading="lazy" width="380" height="245"/>
-                                </a>
-                            </div>
-                            <div className="card_info_top">
-                                <h3 className="card_title">
-                                    <a target="_blank" rel='noopener noreferrer' href={t(`${liveLink}`)}>
-                                        {t(`${title}`)}
-                                    </a>
-                                </h3>
-                            </div>
-                            <hr className="card_hr"/>
-                            <h4 className="card_tech_stack_title">
-                                {t(`${techStackTitle}`)}
-                            </h4>
-                            <div className="card_tech_stack_container">
-                                {tech.map((t, index) => (
-                                    <div className="card_tech_stack" key={index}>
-                                        {t}
-                                    </div>))}
-                            </div>
-                            <div className="projects_card_bottom">
-                                <a target="_blank" rel='noopener noreferrer' href={t(`${liveLink}`)}>
-                                    {t(`${live}`)}
-                                </a>
-                                {viewCodeLink &&
-                                    <a target="_blank" rel='noopener noreferrer' href={t(`${viewCodeLink}`)}>
-                                        {t(`${viewCode}`)}
-                                    </a>
-                                }
-                            </div>
-                        </div>
-                    ))}
+                    <Swiper
+                        slidesPerView={1}
+                        spaceBetween={25}
+                        breakpoints={{
+                            500: {
+                                slidesPerView: 1,
+                            }, 800: {
+                                slidesPerView: 2,
+                            }, 1000: {
+                                slidesPerView: 3,
+                            }
+                        }}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        modules={[Pagination]}
+                        className="mySwiper"
+                    >
+                        {projects.map(({
+                                           id,
+                                           title,
+                                           tech,
+                                           live,
+                                           liveLink,
+                                           viewCode,
+                                           viewCodeLink,
+                                           image
+                                       }) => (
+                            <SwiperSlide key={id}>
+                                <div className="projects_card">
+                                    <div className="projects_card_image_frame">
+                                        <a target="_blank" rel='noopener noreferrer' href={t(`${liveLink}`)}>
+                                            <img src={image} alt="ProjectImage" loading="lazy" width="380"
+                                                 height="245"/>
+                                        </a>
+                                    </div>
+                                    <div className="card_info_top">
+                                        <h3 className="card_title">
+                                            <a target="_blank" rel='noopener noreferrer' href={t(`${liveLink}`)}>
+                                                {t(`${title}`)}
+                                            </a>
+                                        </h3>
+                                    </div>
+                                    <div className="card_tech_stack_container">
+                                        {tech.map((t, index) => (
+                                            <div className="card_tech_stack" key={index}>
+                                                {t}
+                                            </div>))}
+                                    </div>
+                                    <div className="projects_card_bottom">
+                                        <a target="_blank" rel='noopener noreferrer' href={t(`${liveLink}`)}>
+                                            {t(`${live}`)}
+                                        </a>
+                                        {viewCodeLink &&
+                                            <a target="_blank" rel='noopener noreferrer' href={t(`${viewCodeLink}`)}>
+                                                {t(`${viewCode}`)}
+                                            </a>
+                                        }
+                                    </div>
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
                 </div>
             </div>
         </div>
